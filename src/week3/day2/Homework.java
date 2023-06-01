8a321055a8f8d2a96dc741c32c795aa0fca38111
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-06-01 23:21:17 +0600

    week 3 day 2 hometask

---
+++ src/week3/day2/Homework.java
@@ -0,0 +1,53 @@
+package week3.day2;
+
+import java.util.Scanner;
+
+public class Homework {
+    public static void main(String[] args) {
+
+        // #1
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.print("enter week day number: ");
+//        int day = in.nextInt();
+//
+//        switch (day) {
+//            case 1 -> System.out.println("It is Monday");
+//            case 2 -> System.out.println("It is Tuesday");
+//            case 3 -> System.out.println("It is Wednesday");
+//            case 4 -> System.out.println("It is Thursday");
+//            case 5 -> System.out.println("It is Friday");
+//            case 6 -> System.out.println("It is Saturday");
+//            case 7 -> System.out.println("It is Sunday");
+//            default -> System.out.println("Wrong input");
+//        }
+
+        // #2
+//        int count = 10;
+//
+//        while(count >= 1){
+//            if(count % 2 == 0){ //do printing only if count is even
+//                System.out.print(count + " ");
+//            }
+//            count = count - 1;
+//        }
+
+
+        // #3
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.print("enter a number: ");
+//        int num = in.nextInt(); //just named is "num", not N as in the task
+//        int count = 1, sum = 0;
+//
+//        while(count <= num){
+//            if(count % 2 == 0){ //we will calculate the sum if only count is even
+//                sum = sum + count;
+//            }
+//            count = count + 1;
+//        }
+//
+//        System.out.println("sum is: " + sum);
+
+    }
+}
