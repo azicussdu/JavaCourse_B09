b9b2543604708ab60b37e0097d6f68ee7c0d88a4
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-06-02 10:58:02 +0600

    week 3 day 3 hw

--- src/week3/day3/Homework_For.java
+++ src/week3/day3/Homework_For.java
@@ -6,18 +6,51 @@ public class Homework_For {
     public static void main(String[] args) {
 
         // #1
-        Scanner in = new Scanner(System.in);
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.print("enter a number: ");
+//        int num = in.nextInt();
+//        int sum = 0;
+//
+//        for(int i=1; i<=num; i++){
+//            if(i % 2 == 0)
+//                sum = sum + i;
+//        }
+//
+//        System.out.println("sum of even numbers: " + sum);
 
-        System.out.print("enter a number: ");
-        int num = in.nextInt();
-        int sum = 0;
 
-        for(int i=1; i<=num; i++){
-            if(i % 2 == 0)
-                sum = sum + i;
-        }
+        // #2
+//        Scanner in = new Scanner(System.in);
+//
+//        int num, sum = 0;
+//        double average;
+//
+//        System.out.println("enter 5 numbers: ");
+//        for(int i=1; i<=5; i++){
+//            num = in.nextInt();
+//            sum = sum + num;
+//        }
+//
+//        average = sum / 5.0;
+//        System.out.println("average is: " + average);
 
-        System.out.println("sum of even numbers: " + sum);
+
+        // #3
+//        Scanner in = new Scanner(System.in);
+//
+//        int amount, num, countNegative = 0;
+//        System.out.print("enter the amount of numbers to input: ");
+//        amount = in.nextInt();
+//
+//        for(int i=1; i<=amount; i++){
+//            num = in.nextInt();
+//            if(num < 0){
+//                countNegative++;
+//            }
+//        }
+//
+//        System.out.println("You entered " + countNegative + " negative numbers");
 
     }
 }
