f557fcf03716cd32b2b0bdd19f6141a763d39a50
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-06-05 20:49:02 +0600

    week 3 project tasks

---
+++ src/week3/day4/Homework.java
@@ -0,0 +1,52 @@
+package week3.day4;
+
+import java.util.Scanner;
+
+public class Homework {
+    public static void main(String[] args) {
+
+        // #1.
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("enter numbers, to finish enter 0(zero): ");
+//        int num, countPos = 0, countNeg = 0;
+//
+//        while(true){
+//            num = in.nextInt();
+//            if(num == 0)
+//                break;
+//
+//            if(num > 0)
+//                countPos++;
+//            else
+//                countNeg++;
+//        }
+//
+//        if(countPos == countNeg)
+//            System.out.println("Equal");
+//        else
+//            if(countPos > countNeg)
+//                System.out.println("Yes");
+//            else
+//                System.out.println("No");
+
+        // #2.
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("enter one number: ");
+//        int num = in.nextInt();
+//        int sum = 0, lastDigit;
+//
+//        while(true){
+//            if(num == 0)
+//                break;
+//
+//            lastDigit = num % 10;
+//            if(lastDigit % 2 == 1)
+//                sum = sum + lastDigit;
+//
+//            num = num / 10;
+//        }
+//
+//        System.out.println("sum of odd digits: " + sum);
+
+    }
+}
