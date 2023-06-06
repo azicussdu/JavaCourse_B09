710691ec786c45e516cad64e2232822905d8f818
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-06-06 07:49:49 +0600

    week 3 project tasks

---
+++ src/week4/day1/Array_1.java
@@ -0,0 +1,111 @@
+package week4.day1;
+
+import java.util.Scanner;
+
+public class Array_1 {
+    public static void main(String[] args) {
+
+//        int[] arr = {10, 20, 30, 40}; //one way of creating and initializing array
+//
+//        arr[2] = 50;
+//
+//        System.out.println("the size of array is: " + arr.length);
+//
+//        System.out.println(arr[0]);
+//        System.out.println(arr[1]);
+//        System.out.println(arr[2]);
+//        System.out.println(arr[3]);
+//
+//        System.out.print("printing from left to right: ");
+//        for(int i=0; i<arr.length; i++){
+//            System.out.print(arr[i] + " ");
+//        }
+//
+//        System.out.print("\nprinting from right to left: ");
+//        for(int i=arr.length-1; i >= 0; i--){
+//            System.out.print(arr[i] + " ");
+//        }
+//
+//        int sum = 0;
+//        for(int i=0; i<arr.length; i++){
+//            sum = sum + arr[i];
+//        }
+//        System.out.println("\nsum is: " + sum);
+
+
+//        int[] arr = new int[10];  // 10 is size of array
+//
+//        arr[0] = 10;
+//        arr[4] = 20;
+//        arr[8] = 30;
+//
+//        for(int i=0; i<arr.length; i++){
+//            System.out.print(arr[i] + " ");
+//        }
+
+        /*
+        Task: user enters 8 values, store them in array.
+        Find the sum of odd values in an array.
+         */
+
+//        Scanner in = new Scanner(System.in);
+//        int[] arr = new int[8];
+//        int sum = 0;
+//
+//        System.out.println("enter 8 values: ");
+//        for(int i=0; i<arr.length; i++){
+//            arr[i] = in.nextInt();
+//        }
+//
+//        for(int i=0; i<arr.length; i++){
+//            if(arr[i] % 2 == 1)
+//                sum = sum + arr[i];
+//        }
+//
+//        System.out.println("sum of odds is: " + sum);
+
+        /*
+        Task: User enters number N(size of array).
+        Then user enters N numbers.
+        Print the amount of odds and evens in an array.
+
+        input: 5
+        1 4 7 2 6
+
+        output:
+        odds: 2
+        evens: 3
+         */
+
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.print("enter the N: ");
+//        int n = in.nextInt();
+//        int[] arr = new int[n];
+//        int odds = 0, evens = 0;
+//
+//        System.out.print("enter " + n + " values: ");
+//        for(int i=0; i<arr.length; i++){
+//            arr[i] = in.nextInt();
+//        }
+//
+//        for(int i=0; i<arr.length; i++){
+//            if(arr[i] % 2 == 0)
+//                evens++;
+//            else
+//                odds++;
+//        }
+//
+//        System.out.println("evens: " + evens);
+//        System.out.println("odds: " + odds);
+
+    }
+}
+
+
+
+
+
+
+
+
---
+++ src/week4/day1/DoWhile.java
@@ -0,0 +1,34 @@
+package week4.day1;
+
+import java.util.Scanner;
+
+public class DoWhile {
+    public static void main(String[] args) {
+
+        /*
+        User inputs any amount of number, to finish input user enters ZERO.
+        Find sum of numbers.
+         */
+//        Scanner in = new Scanner(System.in);
+//        int num, sum = 0;
+//
+//        do {
+//            System.out.print("enter value: ");
+//            num = in.nextInt();
+//
+//            sum = sum + num;
+//        }while(num != 0);
+//
+//        System.out.println("sum is: " + sum);
+
+
+
+    }
+}
+
+
+
+
+
+
+
