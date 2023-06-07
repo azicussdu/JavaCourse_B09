b550b9694900e8b3444abcd0781d79b8e3e49968
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-06-07 08:01:04 +0600

    week 3 project tasks

---
+++ src/week4/day1/Homework.java
@@ -0,0 +1,94 @@
+package week4.day1;
+
+public class Homework {
+    public static void main(String[] args) {
+
+        // #1
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.print("Enter value for N: ");
+//        int n = in.nextInt();
+//        int sumEvens = 0, sumOdds = 0;
+//
+//        int[] arr = new int[n];
+//
+//        for(int i=0; i<arr.length; i++){
+//            System.out.print("enter array value: ");
+//            arr[i] = in.nextInt();
+//        }
+//
+//        for(int i=0; i<arr.length; i++){
+//            if(arr[i] % 2 == 0)
+//                sumEvens = sumEvens + arr[i];
+//            else
+//                sumOdds = sumOdds + arr[i];
+//        }
+//
+//        if(sumOdds > sumEvens)
+//            System.out.println("No");
+//        else
+//            if(sumEvens > sumOdds)
+//                System.out.println("Yes");
+//            else
+//                System.out.println("Equal");
+
+        // #2
+//        Scanner in = new Scanner(System.in);
+//        Random random = new Random();
+//
+//        System.out.print("enter the N: ");
+//        int n = in.nextInt();
+//        int[] arr = new int[n];
+//
+//        for(int i=0; i<arr.length; i++){
+//            arr[i] = random.nextInt(1, 20);
+//            System.out.print(arr[i] + " ");
+//        }
+//
+//        int sumLeft = 0, sumRight = 0, midIndex = n / 2;
+//
+//        for(int i=0; i < midIndex; i++){
+//            sumLeft = sumLeft + arr[i];
+//        }
+//
+//        if(n % 2 == 1) { //if size of array is odd number
+//            midIndex = midIndex + 1;
+//        }
+//
+//        for(int i=midIndex; i<arr.length; i++){
+//            sumRight = sumRight + arr[i];
+//        }
+//
+//        if(sumRight > sumLeft)
+//            System.out.println("Right side");
+//        else
+//            if(sumLeft > sumRight)
+//                System.out.println("Left side");
+//            else
+//                System.out.println("Equal");
+
+
+        // #3
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.print("Enter value for N: ");
+//        int n = in.nextInt();
+//
+//        int[] arr = new int[n];
+//
+//        System.out.print("enter " + n + " values: ");
+//        for(int i=0; i<arr.length; i++){
+//            arr[i] = in.nextInt();
+//        }
+//
+//        int max = 0;
+//        for(int i=0; i<arr.length; i++){
+//            if(arr[i] > max){
+//                max = arr[i];
+//            }
+//        }
+//
+//        System.out.println("max is: " + max);
+
+    }
+}
