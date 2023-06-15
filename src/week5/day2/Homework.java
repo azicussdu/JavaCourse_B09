5a1c417430b99a26faf93bfe47d67df18f0474d4
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-06-15 07:49:10 +0600

    week 3 project tasks

---
+++ src/week5/day2/Homework.java
@@ -0,0 +1,62 @@
+package week5.day2;
+
+import java.util.Scanner;
+
+public class Homework {
+    public static void main(String[] args) {
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("enter password: ");
+//        String password = in.next();
+//
+//        boolean hasUppercase = false, hasLowercase = false, hasDigit = false, hasPunctuaton = false;
+//        for(int i=0; i<password.length(); i++){
+//            if(password.charAt(i) >= 65 && password.charAt(i) <= 90){
+//                hasUppercase = true;
+//            }
+//            else
+//                if(password.charAt(i) >= 97 && password.charAt(i) <= 122){
+//                    hasLowercase = true;
+//                }
+//                else
+//                    if(password.charAt(i) >= 48 && password.charAt(i) <= 57){
+//                        hasDigit = true;
+//                    }
+//                    else
+//                        if(password.charAt(i) == '.' || password.charAt(i) == '!' || password.charAt(i) == '?'){
+//                            hasPunctuaton = true;
+//                        }
+//        }
+//
+//        if(hasUppercase && hasLowercase && hasDigit && hasPunctuaton && password.length() >= 6){
+//            System.out.println("Valid password");
+//        }
+//        else {
+//            System.out.println("Invalid password");
+//        }
+//
+//        if(hasUppercase == false){
+//            System.out.println("you should have at least one uppercase");
+//        }
+//        if(hasLowercase == false){
+//            System.out.println("you should have at least one lowercase");
+//        }
+//        if(hasDigit == false){
+//            System.out.println("you should have at least one digit");
+//        }
+//        if(hasPunctuaton == false){
+//            System.out.println("you should have at least one punctuation mark");
+//        }
+//        if(password.length() < 6){
+//            System.out.println("the length should be >= 6");
+//        }
+
+    }
+}
+
+
+
+
+
+
+
---
+++ src/week5/day3/String_2.java
@@ -0,0 +1,119 @@
+package week5.day3;
+
+import java.util.Scanner;
+
+public class String_2 {
+    public static void main(String[] args) {
+
+//        String s = "Hello World";
+//        System.out.println(s.charAt(s.length()-1));
+
+        //indexOf,lastIndexOf
+//        String s = "Hello World World World";
+//        System.out.println(s.indexOf('W') + " " + s.lastIndexOf('W'));
+//
+//        int indexOfFirstW = s.indexOf('W');
+//        System.out.println(s.indexOf('W', indexOfFirstW+1));
+
+//        String s = "I love java, java is the best";
+//        int indexOfJava = s.indexOf("java");
+//        System.out.println("index of java word: " + indexOfJava);
+
+        /*
+        User enters email. Check if it is valid or not.
+        Conditions for valid email:
+        - email should have only one '@' sign
+        - last '.' sign should be after '@' sign
+        - at least 1 letter before and after '@'
+        - at least 2 letters after last '.'
+         */
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("enter email: ");
+//        String email = in.next();
+//
+//        boolean emailIsValid = true;
+//
+//        if(email.indexOf('@') == -1 || email.indexOf('.') == -1){
+//            emailIsValid = false;
+//            System.out.println("Invalid, you should have at least one @ sign or period sign");
+//        }
+//
+//        if(email.indexOf('@') != email.lastIndexOf('@')){
+//            emailIsValid = false;
+//            System.out.println("Invalid, you should have only ONE @ sign");
+//        }
+//
+//        if(email.lastIndexOf('.') < email.indexOf('@')){
+//            emailIsValid = false;
+//            System.out.println("Invalid, the period should go after @ sign");
+//        }
+//
+//        if(email.indexOf('@') == 0 || (email.indexOf('@')+1 == email.lastIndexOf('.'))){
+//            emailIsValid = false;
+//            System.out.println("Invalid, there should be at least one letter before and after @ sign");
+//        }
+//
+//        if(email.length() - email.lastIndexOf('.') <= 2){
+//            emailIsValid = false;
+//            System.out.println("Invalid, there should be at least two letters after last period");
+//        }
+//
+//        if(emailIsValid)
+//            System.out.println("Valid email");
+
+
+//        String s = "HellO !? 123 WOrld";
+//        s = s.toLowerCase();
+//        System.out.println("new value of s is: " + s);
+
+//        String s = "HellO !? 123 WOrld";
+//        s = s.toUpperCase();
+//        System.out.println("new value of s is: " + s);
+
+//        String s1 = "Hello";
+//        String s2 = "heLLO";
+//
+//        s1 = s1.toLowerCase();
+//        s2 = s2.toLowerCase();
+//
+//        if(s1.equals(s2)){
+//            System.out.println("Equal");
+//        }
+//        else
+//            System.out.println("Not equal");
+//
+//        System.out.println(s1);
+//        System.out.println(s2);
+
+
+//        String s1 = "Hello";
+//        String s2 = "heLLO";
+//
+//        if(s1.equalsIgnoreCase(s2)){
+//            System.out.println("Equal");
+//        }
+//        else
+//            System.out.println("Not equal");
+//
+//        System.out.println(s1);
+//        System.out.println(s2);
+
+//        Scanner in = new Scanner(System.in);
+//        String s = in.nextLine(); //vs in.next() - which reads only 1 word
+//        System.out.println("value is: " + s);
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
