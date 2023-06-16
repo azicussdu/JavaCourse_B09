9fd02e1f70aeec83d560192a0bfaebcfa8c3d334
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-06-16 08:00:32 +0600

    week 3 project tasks

---
+++ src/week5/day1/Homework.java
@@ -0,0 +1,112 @@
+package week5.day1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Homework {
+    public static void main(String[] args) {
+
+        // #1.
+//        Scanner in = new Scanner(System.in);
+//        Random random = new Random();
+//
+//        System.out.print("enter N and M: ");
+//        int n = in.nextInt();
+//        int m = in.nextInt();
+//
+//        int[][] arr = new int[n][m];
+//
+//        for(int i=0; i<n; i++){
+//            for(int j=0; j<m; j++){
+//                arr[i][j] = random.nextInt(1, 10);
+//                System.out.print(arr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+//
+//        for(int i=0; i<n; i++){
+//            for(int j=0; j<m; j++){
+//                if(arr[i][j] % 2 == 0)
+//                    arr[i][j] = arr[i][j] * 2;
+//                else
+//                    arr[i][j] = arr[i][j] * 3;
+//            }
+//        }
+//
+//        System.out.println("------- after change ----------");
+//        for(int i=0; i<n; i++){
+//            for(int j=0; j<m; j++){
+//                System.out.print(arr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+
+        // #2.
+//        Scanner in = new Scanner(System.in);
+//        Random random = new Random();
+//
+//        System.out.print("enter N and M: ");
+//        int n = in.nextInt();
+//        int m = in.nextInt();
+//
+//        int[][] arr = new int[n][m];
+//
+//        for(int i=0; i<n; i++){
+//            for(int j=0; j<m; j++){
+//                arr[i][j] = random.nextInt(1, 21);
+//                System.out.print(arr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+//
+//        int countOdds = 0, countEvens = 0;
+//
+//        for(int i=0; i<n; i++) {
+//            for (int j=0; j<m; j++) {
+//                if(arr[i][j] % 2 == 1 && i % 2 == 1){
+//                    countOdds++;
+//                }
+//                if(arr[i][j] % 2 == 0 && i % 2 == 0){
+//                    countEvens++;
+//                }
+//            }
+//        }
+//
+//        System.out.println("The amount of odds from odd row indexes: " + countOdds);
+//        System.out.println("The amount of evens from even row indexes: " + countEvens);
+
+        // #3.
+//        Scanner in = new Scanner(System.in);
+//        Random random = new Random();
+//
+//        System.out.print("enter N: ");
+//        int n = in.nextInt();
+//
+//        int[][] arr = new int[n][n];
+//
+//        for(int i=0; i<n; i++){
+//            for(int j=0; j<n; j++){
+//                arr[i][j] = random.nextInt(1, 10);
+//                System.out.print(arr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+//
+//        int sum1 = 0, sum2 = 0;
+//
+//        for(int i=0; i<n; i++){
+//            for(int j=0; j<n; j++){
+//                if(i == j)
+//                    sum1 = sum1 + arr[i][j];
+//
+//                if(i + j == n - 1)
+//                    sum2 = sum2 + arr[i][j];
+//            }
+//        }
+//
+//        System.out.println("main diagonal sum is: " + sum1);
+//        System.out.println("second diagonal sum is: " + sum2);
+
+    }
+}
